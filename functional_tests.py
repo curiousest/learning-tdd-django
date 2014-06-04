af4b30b32f3c8d5f53cd3768b961f445c321cafa
Douglas Hindson <dmhindson@gmail.com>
2014-06-04 10:30:02 +0800

    Input now added to table

--- functional_tests.py
+++ functional_tests.py
@@ -33,18 +33,24 @@ class NewVisitorTest(unittest.TestCase):
 		
 		table = self.browser.find_element_by_id('id_list_table')
 		rows = table.find_elements_by_tag_name('tr')
-		self.assertTrue(
-			any(row.text == '1: Buy peacock feathers' for row in rows),
-			"New to-do item did not appear in table"
-		)
+		self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
 		
 		#There is still a text box inviting her to add another item. She enters "Use peacock feathers to make a fly"
-		self.fail('Finishe the test!')
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox.send_keys('Use peacock feathers to be stupid')
+		inputbox.send_keys(Keys.ENTER)
+
 		#The page updates again, and now shows both items on her list
-
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+		self.assertIn('2: Use peacock feathers to be stupid', [row.text for row in rows])
+		
 		#The user sees that the site has generated a unique URL for their to-do lists
+		self.fail('Finish the test!')
+		
 		#The user visits the URL and sees that the to-do list is still there
-
+		
 		#END
 
 if __name__ == '__main__':
