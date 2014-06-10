fc0d007db025707d4a42fe612c400698554de2e8
Douglas Hindson <dmhindson@gmail.com>
2014-06-10 10:47:32 +0800

    Use Bootstrap to improve layout

---
+++ lists/migrations/0001_initial.py
@@ -0,0 +1,33 @@
+# encoding: utf8
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='List',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, verbose_name='ID', serialize=False)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Item',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, verbose_name='ID', serialize=False)),
+                ('text', models.TextField(default='')),
+                ('list', models.ForeignKey(to='lists.List', to_field='id', default=None)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+    ]
